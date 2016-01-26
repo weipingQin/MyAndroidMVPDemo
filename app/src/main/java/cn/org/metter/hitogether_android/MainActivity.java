90bb2772c5edc4eecac9cefc23ade3c1f78ef7e3
weipingQin <995308963@qq.com>
2016-01-27 07:48:46 +0800

    修改style文件

--- app/src/main/java/cn/org/metter/hitogether_android/MainActivity.java
+++ app/src/main/java/cn/org/metter/hitogether_android/MainActivity.java
@@ -58,8 +58,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
 
